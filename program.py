1a655bd5f8f9e0651309df0fba00489efccc12fe
waitold <chakkggm@gmail.com>
2018-11-15 03:23:11 +0900

    ボタン名の変更を追加

--- program.py
+++ program.py
@@ -1,51 +1,70 @@
 import tkinter as tk 
-import itertools
+from tkinter import ttk
 import os,tkinter.filedialog, tkinter.messagebox
 
-def callback(event,i,j):#サウンド再生
-    print(event.widget["text"])
+class Application(tk.Frame):
 
-def set_sound(event,i,j):#ボタンの名前設定
-    fTyp = [("","*")]
-    iDir = os.path.abspath(os.path.dirname(__file__))
-    tk.messagebox.showinfo('sound button','処理ファイルを選択してください！')
-    file = tk.filedialog.askopenfilename(filetypes = fTyp,initialdir = iDir)
+    def __init__(self, master = None):
+        #コンストラクタ
+        super().__init__(master)
+        self.place()
+        self.button = [[],[],[],[]]
+        self.button_name = [[],[],[],[]]
+        self.file_path = [[],[],[],[]]
+        self.create_widgets()
+        self.read_files()
 
-    tkinter.messagebox.showinfo('sound button',file)
-    filepath =""
-    for i in file:
-        filepath += i
-    filename = os.path.basename(filepath)
-    print(filepath)
-    print(filename)
+    def create_widgets(self):
+        #ウィジェットの作成、配置
+        count = 1
+        for i in range(4):#再生ボタンの作成
+            for j in range(4):
+                self.button[i].append(ButtonProces(count = count))   
+                self.button[i][j].grid(row = i + 1, column = j + 1, padx = 5, pady = 5)
+                count += 1
 
-    entry_frame = tk.Frame(root)
-    entry_frame.place(x = 10, y =400)
-    entry_text = tk.Label(entry_frame,text = "ファイル名")
-    entry_text.pack()
-    sound_name = tk.Entry(entry_frame,width = 60)
-    sound_name.pack()
-    sound_name.insert(tk.END,filename)
-    entry_button = tk.Button(entry_frame,text = "完了",width = 3,height =1)
-    entry_button.pack()
+
+    def read_files(self):
+        #ファイルの読み込み
+        count = 0
+        with open('filepath.txt') as f:
+            for i in range(4):
+                for j in range(4):
+                    self.file_path[i].append(f.readline(count))
+
+class ButtonProces(tk.Button):
+    def __init__(self, master = None, count = None):
+        tk.Button.__init__(self,master = None,text = count, width = 10, height = 4)
+        self.bind("<1>",self.play)
+        self.bind("<3>", self.set_sound)
+        
+    def play(self,event):
+        print(event.widget["text"])
+
+    def set_sound(self,event):
+        fTyp = [("","*")]
+        iDir = os.path.abspath(os.path.dirname(__file__))
+        tk.messagebox.showinfo('sound button','処理ファイルを選択してください！')
+        file = tk.filedialog.askopenfilename(filetypes = fTyp,initialdir = iDir)
+        #tkinter.messagebox.showinfo('sound button',file)
+        
+        filepath =""
+        for i in file:
+            filepath += i
+        filename = os.path.basename(filepath)
+
+        entry_label = tk.Label(text = "ボタン名を入力してください")
+        entry = tk.Entry(width = 30)
+        entry.insert(tk.END,filename)
+        entry_button = tk.Button(text = "保存",command = lambda : self.set_name(event,entry.get()))
+        entry_label.grid(row = 1, column = 1, columnspan = 4,sticky = tk.S)
+        entry.grid(row = 2, column = 1, columnspan = 4, sticky = tk.S)
+        entry_button.grid(row = 2,column = 4)
+
+    def set_name(self,event,filename):
+        event.widget["text"] = filename
 
 
 root = tk.Tk()
-root.title("sound button")
-root.geometry("400x600")
-    
-
-button = [[],[],[],[]]
-count = 1
-
-for i in range(4):#再生ボタンの作成
-    for j in range(4):
-        button[i].append(tk.Button(root, text = count, width = 10, height = 4))   
-        button[i][j].bind("<1>",callback(self,i,j))
-        button[i][j].bind("<3>",set_sound(self,i,j))
-        count += 1
-for i in range(1,5):#再生ボタンの配置
-     for j in range(1,5):
-         button[i-1][j-1].grid(row = i, column = j, padx = 10, pady = 5)
-
-root.mainloop()
+app = Application(master = root)
+app.mainloop()
